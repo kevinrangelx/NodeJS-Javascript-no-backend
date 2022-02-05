7e6bc69b18fab720baff7199491ae87b7ad0cdf0
Kevin Rangel <kevin.rangelpf@gmail.com>
2022-02-05 16:05:55 -0300

    N12 - Refatorando Sistema e Otimizando o Código

--- restful/index.js
+++ restful/index.js
@@ -8,7 +8,7 @@ let app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-consign().include('routes').into(app);
+consign().include('routes').include('utils').into(app);
 
 app.listen(3000, '127.0.0.1', ()=>{
     console.log('servidor rodando');
