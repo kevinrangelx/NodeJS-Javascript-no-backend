20fadf57b77469a5e70620627a6449fabede7ab3
Kevin Rangel <kevin.rangelpf@gmail.com>
2022-02-05 17:11:02 -0300

    N14 - Editando um usuário

--- restful/routes/users.js
+++ restful/routes/users.js
@@ -20,7 +20,11 @@ module.exports = (app)=>{
     });
     route.post((req, res)=>{
         db.insert(req.body,(err,user)=>{
-            
+            if (err){
+                app.utils.error.send(err, req, res);
+            } else {
+                res.status(200).json(user);
+            }
         });
     })
     let routeId = app.route('/users/:id');
@@ -31,6 +35,15 @@ module.exports = (app)=>{
             } else {
                 res.status(200).json(user);
             }
+    });
+});
+    routeId.put((req, res)=>{
+        db.update({_id:req.params.id}, req.body, err=>{
+            if (err){
+                app.utils.error.send(err, req, res);
+            } else {
+                res.status(200).json(Object.assign(req.params, req.body));
+            }
         });
-    })
+});
 };
