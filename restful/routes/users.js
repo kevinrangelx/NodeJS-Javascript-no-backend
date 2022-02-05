d472cc93cf597654a44927fe0f452c4849c19ee3
Kevin Rangel <kevin.rangelpf@gmail.com>
2022-02-04 21:09:47 -0300

    N07 - Separando Rotas do Arquivo Principal

---
+++ restful/routes/users.js
@@ -0,0 +1,23 @@
+let express = require('express');
+let routes = express.Router();
+
+routes.get('/', (req, res)=>{
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type','application/json');
+    res.json({
+        users:[{
+            name: 'Kevin',
+            email: 'kevin.rangelpf@gmail.com',
+            id:1
+            }]
+        });
+});
+routes.get('/admin',(req, res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type','application/json');
+    res.json({
+        users:[]
+        });
+})
+module.exports = routes;
