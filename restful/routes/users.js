d89262ba505ce17d1cd5a412665ff48eda497c49
Kevin Rangel <kevin.rangelpf@gmail.com>
2022-02-05 15:51:45 -0300

    N11 - Listando os usuários do banco NeDB

--- restful/routes/users.js
+++ restful/routes/users.js
@@ -7,15 +7,20 @@ module.exports = (app)=>{
     
     app.get('/users', (req, res)=>{
 
-        res.statusCode = 200;
-        res.setHeader('Content-Type','application/json');
-        res.json({
-            users:[{
-                name: 'Kevin',
-                email: 'kevin.rangelpf@gmail.com',
-                id:1
-                }]
-            });
+        db.find({}).sort({name:1}).exec((err, users)=>{
+            if (err){
+                console.log('error: ${err}');
+                res.status(400).json({
+                    error:err
+                });
+            }else{
+                res.statusCode = 200;
+                res.setHeader('Content-Type','application/json');
+                res.json({
+                    users
+                    }); 
+            }
+        });   
     });
     app.post('/users',(req, res)=>{
         db.insert(req.body,(err,user)=>{
@@ -26,7 +31,6 @@ module.exports = (app)=>{
                 });
             } else {
                 res.status(200).json(user);
-
             }
 
         });
